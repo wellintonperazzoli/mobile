6212afa802a72a5f0a14b928798d5ab68c62b3e1
wellinton.chyla <wellinton.chyla@rentcars.local>
2014-08-25 10:51:31 -0300

    Alterações adicionando bootstrap.

--- app/js/app.js
+++ app/js/app.js
@@ -10,7 +10,7 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/home', {templateUrl: 'partials/partial1.html', controller: 'HomeCtrl'});
-  $routeProvider.when('/teste', {templateUrl: 'partials/partial2.html', controller: 'TesteCtrl'});
+  $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'HomeCtrl'});
+  $routeProvider.when('/teste', {templateUrl: 'partials/teste.html', controller: 'TesteCtrl'});
   $routeProvider.otherwise({redirectTo: '/home'});
 }]);
